27adeb82239cfd6a5dc94eab5d5ab0cc7e993d82
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2022-08-06 13:57:44 -0400

    make it comparable and printable

--- src/main/java/io/github/pixee/codetf/CodeTFChange.java
+++ src/main/java/io/github/pixee/codetf/CodeTFChange.java
@@ -3,6 +3,7 @@ package io.github.pixee.codetf;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Map;
+import java.util.Objects;
 
 /** Describes a "change" in a CCF report. */
 public final class CodeTFChange {
@@ -46,5 +47,37 @@ public final class CodeTFChange {
 
   public int getLineNumber() {
     return lineNumber;
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    final CodeTFChange that = (CodeTFChange) o;
+    return lineNumber == that.lineNumber
+        && Objects.equals(category, that.category)
+        && Objects.equals(description, that.description)
+        && Objects.equals(properties, that.properties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(lineNumber, category, description, properties);
+  }
+
+  @Override
+  public String toString() {
+    return "CodeTFChange{"
+        + "lineNumber="
+        + lineNumber
+        + ", category='"
+        + category
+        + '\''
+        + ", description='"
+        + description
+        + '\''
+        + ", properties="
+        + properties
+        + '}';
   }
 }
