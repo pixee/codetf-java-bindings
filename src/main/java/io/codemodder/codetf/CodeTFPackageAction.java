24c3c7f2d2480efdca2478f8b98019003e8b1a79
Johnathan Gilday <johnathan.gilday@pixee.ai>
2024-03-15 11:11:54 -0400

    ✅ Add Test for Enum Case Sensitivity

--- src/main/java/io/codemodder/codetf/CodeTFPackageAction.java
+++ src/main/java/io/codemodder/codetf/CodeTFPackageAction.java
@@ -48,4 +48,37 @@ public final class CodeTFPackageAction {
   public CodeTFPackageActionType getAction() {
     return action;
   }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    final CodeTFPackageAction that = (CodeTFPackageAction) o;
+
+    if (!packageUrl.equals(that.packageUrl)) return false;
+    if (result != that.result) return false;
+    return action == that.action;
+  }
+
+  @Override
+  public int hashCode() {
+    int result1 = packageUrl.hashCode();
+    result1 = 31 * result1 + result.hashCode();
+    result1 = 31 * result1 + action.hashCode();
+    return result1;
+  }
+
+  @Override
+  public String toString() {
+    return "CodeTFPackageAction{"
+        + "packageUrl='"
+        + packageUrl
+        + '\''
+        + ", result="
+        + result
+        + ", action="
+        + action
+        + '}';
+  }
 }
